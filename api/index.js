854b8d588b2b38f544addfd73db1806b5ea6dae6
Alertse Tavares <alertseapt@gmail.com>
2025-08-19 10:27:47 -0300

    Preparar backend para deploy na Vercel - Adicionar configurações serverless, variáveis de ambiente e documentação

---
+++ api/index.js
@@ -0,0 +1,7 @@
+// Entry point para Vercel Serverless Functions
+// Este arquivo exporta o app Express existente para a Vercel
+
+const app = require('../app');
+
+// Exportar o app para a Vercel usar como serverless function
+module.exports = app;
